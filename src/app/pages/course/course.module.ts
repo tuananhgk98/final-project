7b8bfa149e1d693b95cbcd84d919733442bd37a9
tuananh <anhtuangk98@gmail.com>
2020-05-28 17:32:47 +0700

    update

--- src/app/pages/course/course.module.ts
+++ src/app/pages/course/course.module.ts
@@ -6,7 +6,6 @@ import { CourseListComponent } from './components/course-list/course-list.compon
 import { CourseDetailComponent } from './components/course-detail/course-detail.component';
 import { LessonsComponent } from './components/course-detail/lessons/lessons.component';
 import { FormsModule } from '@angular/forms';
-// import { MonacoEditorModule, MONACO_PATH } from '@materia-ui/ngx-monaco-editor';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
 @NgModule({
   declarations: [CourseListComponent, CourseDetailComponent, LessonsComponent],
@@ -14,15 +13,8 @@ import { MonacoEditorModule } from 'ngx-monaco-editor';
     CommonModule,
     CourseRoutingModule,
     FormsModule,
-    // MonacoEditorModule,
     MonacoEditorModule.forRoot()
   ],
-  // providers: [
-  //   {
-  //     provide: MONACO_PATH,
-  //     useValue: 'https://unpkg.com/monaco-editor@0.19.3/min/vs'
-  //   }
-  // ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
     NO_ERRORS_SCHEMA
